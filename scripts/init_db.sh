d2e290c3b52359000f7c52291d0015150e835d7b
mgxnch <mgxnch@gmail.com>
2025-09-07 22:58:45 +0800

    feat: add basic script to start postgres in docker

---
+++ scripts/init_db.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+# Check if a custom value has been set, otherwise default to its default value
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+# Launch postgres using Docker
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
