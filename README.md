0264c6d6020813c4d9025320264814412887ea7e
mgxnch <mgxnch@gmail.com>
2026-07-25 18:27:35 +0800

    feat: add endpoints

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zero2Prod
+
+## Features
+
+1. Blog reader to subscribe to a newletter.
+
+## Backend
+
+Using `axum` as the web server.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::extract::Path;
+use axum::{Router, routing::get};
+
+async fn greet(Path(name): Path<String>) -> String {
+    format!("Hello {}", name)
+}
+
+#[tokio::main]
+async fn main() {
+    // Initialise a router
+    let app = Router::new()
+        .route("/", get(|| async { "hello world!" }))
+        .route("/{name}", get(greet));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
